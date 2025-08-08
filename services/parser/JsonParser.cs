3b88b28d4e89eaa4bf9695a5ecf3a8fc24dcd97c
amrbadran <badran.amr2003@gmail.com>
2025-08-08 15:56:15 +0300

    Implement parsing logic for both xml and json

--- services/parser/JsonParser.cs
+++ services/parser/JsonParser.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Real_time_weather_monitoring_system.models;
 
 namespace Real_time_weather_monitoring_system.services.parser;
@@ -10,8 +11,11 @@ public class JsonParser : IParser
     /// </summary>
     /// <param name="weatherData"></param>
     /// <returns>WeatherData Object</returns>
+    /// <exception cref="ArgumentNullException">Thrown when argument is null</exception>
+    /// <exception cref="JsonException">Thrown when it can't convert the string</exception>
     public WeatherData Parse(string weatherData)
     {
-        throw new NotSupportedException();
+        if (weatherData == null) throw new ArgumentNullException();
+        return JsonSerializer.Deserialize<WeatherData>(weatherData);
     }
 }
--- services/parser/XmlParser.cs
+++ services/parser/XmlParser.cs
@@ -1,16 +1,24 @@
+using System.Xml.Serialization;
 using Real_time_weather_monitoring_system.models;
 
 namespace Real_time_weather_monitoring_system.services.parser;
 
 public class XmlParser : IParser
-{   
+{
     /// <summary>
     /// Convert Xml String To WeatherData Object
     /// </summary>
     /// <param name="weatherData"></param>
     /// <returns>WeatherData Object</returns>
-    public WeatherData Parse(string weatherData)
+    /// <exception cref="ArgumentNullException">Thrown when argument is null</exception>
+    /// <exception cref="InvalidOperationException">Thrown when it can't convert the string</exception>
+    public WeatherData Parse(string? weatherData)
     {
-        throw new NotImplementedException();
+        if (weatherData == null) throw new ArgumentNullException();
+        var serializer = new XmlSerializer(typeof(WeatherData));
+        using (StringReader reader = new StringReader(weatherData))
+        {
+            return (WeatherData)serializer.Deserialize(reader);
+        }
     }
 }
