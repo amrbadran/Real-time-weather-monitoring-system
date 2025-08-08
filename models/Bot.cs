0cb8f8ef4756c5b492f6a8e6972a28e353c0a7cf
amrbadran <badran.amr2003@gmail.com>
2025-08-08 18:25:22 +0300

    Fix Bot.cs

--- models/Bot.cs
+++ models/Bot.cs
@@ -2,7 +2,7 @@ namespace Real_time_weather_monitoring_system.models;
 
 public record struct Bot(
     bool? Enabled,
-    string? HumidityThreshold,
-    string? TemperatureThreshold,
+    double? HumidityThreshold,
+    double? TemperatureThreshold,
     string? Message
 );
